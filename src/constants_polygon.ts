81e80c40e8b3e78d2e9c8a4e98d41b4edd46c918
Keiron <keiron.gulrajani@gmail.com>
2023-11-22 13:21:08 +0100

    Added evolv3 nft address

--- src/constants_polygon.ts
+++ src/constants_polygon.ts
@@ -7,6 +7,10 @@ export let MURALL_MONTAGE_NFT_ADDRESS = Address.fromString(
   '0xb200c550749c8f996376e7dbf56b368bc0967b8e'
 )
 
+export let MURALL_EVOLV3_NFT_ADDRESS = Address.fromString(
+  '0x962f182c8e49fc2d9e4385202bdbe5f7a8578415'
+)
+
 export function isValidEmoteAddress (address: Address): bool {
-  return address == MURALL_NFT_ADDRESS || address == MURALL_MONTAGE_NFT_ADDRESS
+  return address == MURALL_NFT_ADDRESS || address == MURALL_MONTAGE_NFT_ADDRESS || address == MURALL_EVOLV3_NFT_ADDRESS
 }
